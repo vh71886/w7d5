4611e4097109c30f71d2deae4f7c4376a9d4c467
nilshen <linshen88@gmail.com>
2022-05-13 12:15:38 -0400

    w7d5_phaseII

---
+++ reddit_clone/db/migrate/20220513160631_create_subs.rb
@@ -0,0 +1,14 @@
+class CreateSubs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :subs do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.integer :user_id, null: false
+      
+
+      t.timestamps
+    end
+    add_index :subs, :user_id
+    #Ex:- add_index("admin_users", "username")
+  end
+end
